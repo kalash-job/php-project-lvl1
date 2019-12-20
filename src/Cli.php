2c78e5e461f4e44eb752f0a8b485a77c75086436
Nikolay Kalashnikov <kalash-job@yandex.ru>
2019-12-20 11:15:25 +0300

    Подключение PHP_CodeSniffer к проекту

--- src/Cli.php
+++ src/Cli.php
@@ -2,13 +2,13 @@
 
 namespace BrainGames\Cli;
 
-use function \cli\line;
-use function \cli\prompt;
+use function cli\line;
+use function cli\prompt;
 
 function run()
 {
-  line('Welcome to the Brain Games!');
-  $name = prompt('May I have your name?');
-  line("Hello, %s!", $name);
-  return;
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    return;
 }
