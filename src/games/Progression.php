3e90b935f0486604f0ff6e124b70075b2f7db823
Nikolay Kalashnikov <kalash-job@yandex.ru>
2020-01-14 04:55:53 +0300

    Скорректировать игру brain-progression под архитектуру Исполн.файл-Игра-Движок

--- src/games/Progression.php
+++ src/games/Progression.php
@@ -2,29 +2,38 @@
 
 namespace BrainGames\Progression;
 
+use function BrainGames\Game\playBrainGame;
+
 /**
- * Функция возвращает вопрос и правильный ответ для задания brain-progression
+ * Функция создает задание, вопросы и ответы к заданию brain-progression
  *
- * Функция создает случайную прогрессию с пропуском в задании brain-progression
- * и возвращает массив с прогрессией и правильным ответом к заданию.
+ * Функция формирует текст задания, три случайных прогрессии с пропуском, рассчитывает
+ * ответы и передает в "движок" задание, массивы с вопросами и ответами.
  *
- * @return array
+ * @return void
  */
-function getTaskProgression(): array
+function getTaskProgression()
 {
-    $length = 10;
-    $difference = mt_rand(1, 10);
-    $numberOfHiddenMember  = mt_rand(0, $length - 1);
-    $progression = [];
-    $firstMember = mt_rand(1, 10);
-    for ($i = 0; $i < $length; $i++) {
-        if ($i !== $numberOfHiddenMember) {
-            $progression[] = $firstMember + $difference * $i;
-        } else {
-            $progression[] = '..';
-            $correctAnswer = (string) ($firstMember + $difference * $i);
+    $task = 'What number is missing in the progression?';
+    $countOfQuestions = 3;
+    $expressionsQuestions = [];
+    $correctAnswers = [];
+    for ($i = 0; $i < $countOfQuestions; $i++) {
+        $length = 10;
+        $difference = mt_rand(1, 10);
+        $numberOfHiddenMember  = mt_rand(0, $length - 1);
+        $progression = [];
+        $firstMember = mt_rand(1, 10);
+        for ($j = 0; $j < $length; $j++) {
+            if ($j !== $numberOfHiddenMember) {
+                $progression[] = $firstMember + $difference * $j;
+            } else {
+                $progression[] = '..';
+                $correctAnswers[] = (string) ($firstMember + $difference * $j);
+            }
         }
+        $expressionsQuestions[] = implode(' ', $progression);
     }
-    $expressionQuestion = implode(' ', $progression);
-    return [$expressionQuestion, $correctAnswer];
+    playBrainGame($task, $expressionsQuestions, $correctAnswers);
+    return;
 }
